e07ac459f366c22b32246d525f20df082896213c
tails5555 <tails5555@naver.com>
2018-04-08 01:23:18 +0900

    Skeleton Chattin' REST API

---
+++ src/main/java/net/kang/Example032Application.java
@@ -0,0 +1,12 @@
+package net.kang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Example032Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Example032Application.class, args);
+	}
+}

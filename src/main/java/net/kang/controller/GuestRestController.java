9f64ce1c5e2a2f59f124dfdc44be363841cde578
tails5555 <tails5555@naver.com>
2018-04-10 11:10:01 +0900

    Add JWT Token Controller in guest/login

--- src/main/java/net/kang/controller/GuestRestController.java
+++ src/main/java/net/kang/controller/GuestRestController.java
@@ -1,5 +1,33 @@
 package net.kang.controller;
 
-public class GuestRestController {
+import java.io.IOException;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.kang.domain.User;
+import net.kang.model.LoginForm;
+import net.kang.service.TokenAuthenticationService;
+import net.kang.service.UserService;
+
+@RestController
+@CrossOrigin
+@RequestMapping("guest")
+public class GuestRestController {
+	@Autowired UserService userService;
+	@Autowired TokenAuthenticationService tokenAuthenticationService;
+	@PostMapping("login")
+	public ResponseEntity<?> login(HttpServletResponse response, @RequestBody LoginForm loginForm) throws IOException{
+		User loginUser=userService.login(loginForm.getUserId(), loginForm.getPassword());
+		if(loginUser==null) return new ResponseEntity<String>("", HttpStatus.UNAUTHORIZED);
+		return new ResponseEntity<String>(tokenAuthenticationService.addAuthentication(response, loginUser), HttpStatus.OK);
+	}
 }
